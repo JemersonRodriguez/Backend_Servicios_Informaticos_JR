c479691588b195c870cf5bf0014a519985115726
jrodriguez <JEMERSON.RODRIGUEZ@ucr.ac.cr>
2025-12-09 21:59:19 -0600

    Feat: Actualizacion de repo remoto con respecto a lo desarrollado en local

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Backend_SSR_Servicios_Informaticos_JR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
