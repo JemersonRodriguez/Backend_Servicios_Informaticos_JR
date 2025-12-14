c45132fc4fa3616ff96842e915dad91f962251c4
jrodriguez <JEMERSON.RODRIGUEZ@ucr.ac.cr>
2025-12-14 16:18:09 -0600

    Feat: Interface capa de servicio que define el metodo de envio de correo para su posterior implementacion funcionando como capa de abstraccion

---
+++ Services/IEnviarCorreoService.cs
@@ -0,0 +1,10 @@
+﻿using Backend_SSR_Servicios_Informaticos_JR.Dtos;
+using Backend_SSR_Servicios_Informaticos_JR.Response;
+
+namespace Backend_SSR_Servicios_Informaticos_JR.Services
+{
+    public interface IEnviarCorreoService
+    {
+        Task<ResponseData> EnviarCorreoAsync(EnvioCorreoDto dataCorreo);
+    }
+}
