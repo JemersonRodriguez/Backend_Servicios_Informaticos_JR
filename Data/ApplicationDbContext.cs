fb9d51dff1301cdd8b54a5bf2408cfc4376b000c
jrodriguez <JEMERSON.RODRIGUEZ@ucr.ac.cr>
2025-12-15 00:35:21 -0600

    Feat: Creacion del context para configuracion de modelos y definiciones de tablas y relaciones usando Fluent API

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend_SSR_Servicios_Informaticos_JR.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define DbSets for your entities here
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity mappings here
+        }
+
+    }
+}
