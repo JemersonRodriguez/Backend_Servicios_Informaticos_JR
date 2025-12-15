1a55b2fffef553e3ac1b453690e8966b117e28a0
jrodriguez <JEMERSON.RODRIGUEZ@ucr.ac.cr>
2025-12-15 00:36:28 -0600

    Feat: Nuevos servicios añadidos al proyecto (Entity Framework, Design y Sql Server)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
+using Backend_SSR_Servicios_Informaticos_JR.Data;
 using Backend_SSR_Servicios_Informaticos_JR.Services;
+using Microsoft.EntityFrameworkCore;
 using Resend;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+#region Registro de Servicios
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IEnviarCorreoService, EnviarCorreoService>();
-#region
 
 // MVC
 builder.Services.AddControllersWithViews();
@@ -21,14 +23,17 @@ builder.Services.AddHttpClient<ResendClient>();
 builder.Services.Configure<ResendClientOptions>(options =>
 {
     options.ApiToken =
-        builder.Configuration["Resend:ApiKey"]
-        ?? Environment.GetEnvironmentVariable("RESEND_APITOKEN");
+        builder.Configuration["Resend:ApiKey"] ?? Environment.GetEnvironmentVariable("RESEND_APITOKEN");
 });
 
 // Registro DI
 builder.Services.AddTransient<IResend, ResendClient>();
+builder.Services.AddDbContext<ApplicationDbContext>(options=> { 
+    options.UseSqlServer(connectionString);
+});
 
-//End Resend Services
+
+//End Services
 
 #endregion
 
